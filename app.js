00bdf4c849c21ba84955d2d4063d2fd833e7468f
Jeff Irwin <jeff.irwin@live.com>
2017-07-25 11:36:16 -0500

    update to use public folder

--- app.js
+++ app.js
@@ -2,12 +2,12 @@ const express = require('express');
 
 const app = express();
 
-app.get('/', function(req, res) {
-  res.send('hello world');
-});
+app.use(express.static('public'));
+
+// app.get('/', function(req, res) {
+//   res.send('hello world');
+// });
 
 app.listen(3001, function() {
   console.log('Listening to port 3001');
 });
-
-app.use(express.static('public'));
